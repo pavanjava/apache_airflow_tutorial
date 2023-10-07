14ff28a9206e0840e33db02d921ef28bf8a11f1f
Pavan Mantha <manthapavankumar11@gmail.com>
2023-10-07 08:43:25 +0530

    -modifed the filename, -read the data from local storage instead of azure

---
+++ src/dags/task_flow_demo.py
@@ -0,0 +1,29 @@
+import json
+import pandas as pd
+from datetime import datetime
+from airflow.decorators import task, dag
+
+
+@dag(dag_id="housing_etl_workflow", schedule=None, start_date=datetime(2023, 10, 7), catchup=False)
+def city_population_etl_workflow():
+    @task()
+    def extract():
+        df = pd.read_csv("/Users/pavanmantha/Desktop/IndianHouses.csv")
+        return df
+
+    @task()
+    def transform(data):
+        data = pd.DataFrame(data=data)
+        filtered_property = data[data["Transaction"] == "New_Property"]
+        return filtered_property
+
+    @task()
+    def load(property):
+        print(property)
+
+    df = extract()
+    new_properties = transform(data=df)
+    load(property=new_properties)
+
+
+city_population_etl_workflow()
--- src/dags/utils/readAdlsGen2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-source_url = "https://airflowstorage001.blob.core.windows.net/generic-csv-container/IndianHouses.csv?sp=r&st=2023-10-06T15:57:13Z&se=2023-10-06T23:57:13Z&spr=https&sv=2022-11-02&sr=b&sig=9AdQ2jVPjQBwADgKo4BD%2BaYIWZ%2FAgCbPn%2BFvQR9QAss%3D"
-
-
-data = pd.read_csv(source_url)
-print(data[data["Transaction"] == "New_Property"])
